2d4a038950283c598c5f034f120ba09d425778ea
Justin Buchanan <justbuchanan@gmail.com>
2016-12-21 15:54:58 -0800

    dummy part handler works!

--- go/src/github.com/justbuchanan/inventory2-server/main.go
+++ go/src/github.com/justbuchanan/inventory2-server/main.go
@@ -5,11 +5,35 @@ import (
     "html"
     "log"
     "net/http"
+
+    "encoding/json"
+    "github.com/gorilla/mux"
 )
 
+type Part struct {
+    Id string
+    Brief string
+    Description string
+    Quantity uint64
+}
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    router.HandleFunc("/part/{partId}", PartHandler)
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    path := html.EscapeString(r.URL.Path)
+    fmt.Fprintf(w, "Hello, %q", path)
+    fmt.Printf("GET %q\n", path)
+}
+
+func PartHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    partId := vars["partId"]
+
+    p := Part{Id: partId, Brief: "M3 x 12mm screws", Description: "", Quantity: 75}
+    json.NewEncoder(w).Encode(p)
 }

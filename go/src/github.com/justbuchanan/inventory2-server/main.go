93e268350d1b9f05a7adb04f97a3e653e920cfa9
Justin Buchanan <justbuchanan@gmail.com>
2016-12-21 16:20:17 -0800

    labelgen works

--- go/src/github.com/justbuchanan/inventory2-server/main.go
+++ go/src/github.com/justbuchanan/inventory2-server/main.go
@@ -5,6 +5,8 @@ import (
     "html"
     "log"
     "net/http"
+    "os/exec"
+    "bytes"
 
     "encoding/json"
     "github.com/gorilla/mux"
@@ -21,12 +23,13 @@ func main() {
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
     router.HandleFunc("/part/{partId}", PartHandler)
+    router.HandleFunc("/part/{partId}/label", PartLabelHandler)
+    fmt.Println("Inventory api listening on port 8080")
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
     path := html.EscapeString(r.URL.Path)
-    fmt.Fprintf(w, "Hello, %q", path)
     fmt.Printf("GET %q\n", path)
 }
 
@@ -36,4 +39,27 @@ func PartHandler(w http.ResponseWriter, r *http.Request) {
 
     p := Part{Id: partId, Brief: "M3 x 12mm screws", Description: "", Quantity: 75}
     json.NewEncoder(w).Encode(p)
+}
+
+func PartLabelHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    partId := vars["partId"]
+    p := Part{Id: partId, Brief: "M3 x 12mm screws", Description: "", Quantity: 75}
+
+    cmd := exec.Command("/home/justin/src/justin/dymo-python/main.py",
+        p.Brief,
+        "https://inventory.justbuchanan.com/part/" + p.Id,
+        "--bbox",
+        "--size=small",
+        "--output=/tmp/label.pdf")
+    var out bytes.Buffer
+    cmd.Stdout = &out
+    err := cmd.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf(string(out.Bytes()))
+
+    json.NewEncoder(w).Encode(p)
+    fmt.Fprintln(w, "label")
 }

e75e17c9bd6c1aefa4dda37698d9a84a3719c063
Justin Buchanan <justbuchanan@gmail.com>
2016-12-22 00:00:40 -0800

    ng generate component part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PartComponent } from './part/part.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PartComponent
   ],
   imports: [
     BrowserModule,

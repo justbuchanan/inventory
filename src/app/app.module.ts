549034ea316c69a9975945d27e483bceb2d38b45
Justin Buchanan <justbuchanan@gmail.com>
2016-12-22 17:22:52 -0800

    added + button with md-icon md-fab and material design icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { MdCardModule } from '@angular2-material/card';
 import { MdToolbarModule } from '@angular2-material/toolbar';
 import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
 
 import { AppComponent } from './app.component';
 import { PartComponent } from './part/part.component';
@@ -23,8 +24,11 @@ import { PartEditorComponent } from './part-editor/part-editor.component';
     MdCardModule,
     MdToolbarModule,
     MdButtonModule,
+    MdIconModule,
   ],
-  providers: [],
+  providers: [
+    MdIconRegistry,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
